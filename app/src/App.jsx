c120937deb8988de394c3245b4d7639c1a36ba12
Gabriel <stewarti3@yahoo.com>
2025-02-06 00:07:37 +0100

    import components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 import NavBar from './components/NavBar'
+import StatusBar from './components/StatusBar';
+import GameInstructions from './components/GameInstructions';
+import ColorBars from './components/ColorBars';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className='w-screen flex flex-col'>
       <NavBar />
+      <StatusBar />
+      <GameInstructions />
+      <ColorBars />
     </div>
   )
 }
