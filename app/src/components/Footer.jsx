33cc8e3fadc8c104cc12de211205b35b50362ca8
Gabriel <stewarti3@yahoo.com>
2025-02-06 00:50:08 +0100

    update: footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
 
   return (
     <div className='w-full h-[3.5rem] flex flex-row px-8 py-4 mt-auto border-t border-neutral-900 border-opacity-10 item-center bg-white'>
-      <div className='flex items-center mx-auto gap-2'>
+      <div className='flex items-center px-8'>
         <p>
-          KolorGuess! &copy; {currentYear} by
+          &copy; {currentYear} <a href="https://i.on-fleek.com" target="_blank" rel="noopener noreferrer" className='font-bold hover:opacity-60'>
+            Gabriel Isaac
+          </a>
         </p>
-        <a href="https://i.on-fleek.com" target="_blank" rel="noopener noreferrer" className='font-bold hover:opacity-60'>
-          Gabriel Isaac
-        </a>
+
       </div>
     </div>
   )
